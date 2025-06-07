baaff2fccf89fe0500d2b3d708baec29071179e0
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-06 21:38:48 -0300

    adiciona coluna de referencia do eixo Y com os números de 1 a 10

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,11 +3,12 @@
 #include <time.h>
 
 void mostraTabuleiro(int tabuleiro[10][10]) { //automatiza a impressão do tabuleiro
-    for (int i = 0; i < 10; i++){ //imprime linha refencia do eixo X
+    for (int i = 0; i < 10; i++){ //imprime linha referencia do eixo X
         printf(" %c", 'A' + i);
     }
     printf("\n");
     for (int i = 0; i < 10; i++){ //imprime matriz 10 x 10
+        printf("%d. ", i + 1); //imprime coluna referencia do eixo y
         for (int j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]);
         }
