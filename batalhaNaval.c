62e560e3337982c51df621eb86e0c3b98c7318d5
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-06 21:07:34 -0300

    Adiciona .gitignore

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-int main() {
-    int tabuleiro[10][10] = {0};
-
+void mostraTabuleiro(int tabuleiro[10][10]){
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
@@ -14,6 +11,14 @@ int main() {
         }
         printf("\n");
     }
+}
+
+int main() {
+    int tabuleiro[10][10] = {0};
+
+    mostraTabuleiro(tabuleiro);
+
+    
 
     return 0;
 }
