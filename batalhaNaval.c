999c037dcdd07d3e36786b1fa734b532b16b824a
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-07 00:03:07 -0300

    reverte função tabuleiro para inteiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,15 +4,15 @@
 
 #define QUADRADO 10 //define tamanho da matriz quadrada
 
-void montaTabuleiro(char tabuleiro[QUADRADO][QUADRADO]) {
-    for (int i = 0; i < QUADRADO; i++){ //define todos os índices como '0'
+void montaTabuleiro(int tabuleiro[QUADRADO][QUADRADO]) {
+    for (int i = 0; i < QUADRADO; i++){ //define todos os índices como 0
         for (int j = 0; j < QUADRADO; j++){
-            tabuleiro[i][j] = '0';
+            tabuleiro[i][j] = 0;
         }
     }
 }
 
-void mostraTabuleiro(char tabuleiro[QUADRADO][QUADRADO]) { //automatiza a impressão do tabuleiro
+void mostraTabuleiro(int tabuleiro[QUADRADO][QUADRADO]) { //automatiza a impressão do tabuleiro
     printf("   "); //espaçamento da primeira linha
     for (int i = 0; i < QUADRADO; i++){ //imprime linha referencia do eixo X
         printf(" %c", 'A' + i);
@@ -21,7 +21,7 @@ void mostraTabuleiro(char tabuleiro[QUADRADO][QUADRADO]) { //automatiza a impres
     for (int i = 0; i < QUADRADO; i++){ //imprime matriz 10 x 10
         printf("%2d: ", i + 1); //imprime coluna referencia do eixo y
         for (int j = 0; j < QUADRADO; j++){
-            printf("%c ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
@@ -31,20 +31,25 @@ int main() {
     printf("\n"); //quebra de página
 
     //monta tabuleiro
-        char tabuleiro[QUADRADO][QUADRADO]; //define tamanho da matriz
+        int tabuleiro[QUADRADO][QUADRADO]; //define tamanho da matriz
         montaTabuleiro(tabuleiro);
     
     //define posicao do navio um, sempre horizontal
         int navioUmY = 3, navioUmX = 4;
         for (int i = 0; i < 3; i++){
-            tabuleiro[navioUmY][navioUmX + i] = 'X';
+            tabuleiro[navioUmY][navioUmX + i] = 3;
         }
     
-    //define posicao do navio um, sempre horizontal
+    //define posicao do navio dois, sempre horizontal
         int navioDoisY = 5, navioDoisX = 7;
         for (int i = 0; i < 3; i++){
-            tabuleiro[navioDoisY + i][navioDoisX] = 'X';
+            tabuleiro[navioDoisY + i][navioDoisX] = 3;
         }
+
+    //define posicao do navio três, sempre diagonal principal
+
+    //define posicao do navio quatro, sempre diagonal secundaria
+
 
     //exibe estado atualizado do tabuleiro
         mostraTabuleiro(tabuleiro); //imprime o tabuleiro
