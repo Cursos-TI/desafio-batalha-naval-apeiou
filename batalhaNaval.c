e9d5f24308fba9d37066be10508c89cf98efec75
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-06 21:35:50 -0300

    refatora impressão do tabuleiro como função

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    
-    int index;
-    int tabuleiro[10][10] = {0};
-
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
+void mostraTabuleiro(int tabuleiro[10][10]) { //automatiza a impressão do tabuleiro
+    for (int i = 0; i < 10; i++){ //imprime matriz 10 x 10
+        for (int j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
+
+
+int main() {
+    
+    int tabuleiro[10][10] = {0}; //define estado inicial do tabuleiro, todas as casas vazias
+
+    printf("\n"); //quebra de página
+    mostraTabuleiro(tabuleiro); //imprime o tabuleiro
 
     return 0;
 }
+
+// tabuleiro[y][x] tabuleiro [i][j]
