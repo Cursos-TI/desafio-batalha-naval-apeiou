e52d26d6b2fe6e6a09645c4067853b53124bcb5c
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-06 21:20:01 -0300

    elabora exibição do tabuleiro em formato matriz 10 por 10

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,17 @@
 
 int main() {
     
+    int index;
+    int tabuleiro[10][10] = {0};
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
