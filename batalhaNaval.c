4b7b17695f8b408f4450e51530b2d4355d59f41b
Rodrigo Nonaka <rodrigoncg@gmail.com>
2025-06-06 21:41:44 -0300

    corrige alinhamento do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,13 +3,13 @@
 #include <time.h>
 
 void mostraTabuleiro(int tabuleiro[10][10]) { //automatiza a impressão do tabuleiro
-    printf(" "); //espaçamento da primeira linha
+    printf("   "); //espaçamento da primeira linha
     for (int i = 0; i < 10; i++){ //imprime linha referencia do eixo X
         printf(" %c", 'A' + i);
     }
     printf("\n");
     for (int i = 0; i < 10; i++){ //imprime matriz 10 x 10
-        printf("%d. ", i + 1); //imprime coluna referencia do eixo y
+        printf("%2d. ", i + 1); //imprime coluna referencia do eixo y
         for (int j = 0; j < 10; j++){
             printf("%d ", tabuleiro[i][j]);
         }
